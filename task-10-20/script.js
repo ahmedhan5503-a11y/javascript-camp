1ffdab4dedbc976983f6e6445d04bb43e74d252a
Ahmed Hany <ahmedhan5503@gmail.com>
2026-08-04 11:29:33 +0300

    Complete tasks 10 to 17

---
+++ task-10-20/script.js
@@ -0,0 +1,33 @@
+// task1
+let numberOne = 10;
+let numberTwo = 20;
+
+console.log("" + numberOne + numberTwo);
+
+console.log(typeof ("" + numberOne + numberTwo));
+
+console.log(`${numberOne}${numberTwo}`);
+
+console.log(typeof `${numberOne}${numberTwo}`);
+
+console.log(numberTwo + "\n" + numberOne);
+
+console.log(`${numberTwo}
+${numberOne}`);
+// TASK2
+console.log(elzero.innerHTML);
+console.log(typeof elzero);
+// TASK3
+console.log(
+  "I'm In\n" +
+    "\\\\\n" +
+    "Love \\\\ \"\"\" '''\n" +
+    "++ With ++\n" +
+    '\\""\\"""\n' +
+    '""JavaScript""',
+);
+// TASK4
+let a = 21;
+let b = 20;
+
+console.log("_" + a + "_" + b + a + "_" + b + a + "_" + b + "_");
