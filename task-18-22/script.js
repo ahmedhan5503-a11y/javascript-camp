adb9b861d5f95bd733a61ec114fa6ccfc1fb86a9
Ahmed Hany <ahmedhan5503@gmail.com>
2026-08-05 14:56:42 +0300

    fix sscript

--- task-18-22/script.js
+++ task-18-22/script.js
@@ -1,55 +1,67 @@
-// task 1
+// ====================
+// Task 1
+// ====================
 console.log(10 + ((20 * 15) % 3) * 190 - (10 % 400)); // 0
 
-// task 2
-let num = 3;
+// ====================
+// Task 2
+// ====================
+{
+  let num = 3;
 
-// One
-console.log(num + num); // 6
+  // One
+  console.log(num + num); // 6
 
-//  Two
-console.log(num * (true + true)); // 6
+  // Two
+  console.log(num * (true + true)); // 6
 
-//  Three
-console.log(num / (true / (true + true))); // 6
+  // Three
+  console.log(num / (true / (true + true))); // 6
 
-//  Four
-console.log(num << true); // 6
+  // Four
+  console.log(num << true); // 6
 
-//  Five
-console.log(++num + (true + true)); // 6
-num--;
+  // Five
+  console.log(++num + (true + true)); // 6
+  num--;
 
-//  Six
-console.log(num + true + true + true); // 6
+  // Six
+  console.log(num + true + true + true); // 6
+}
 
-// task 3
-let num = "10";
+// ====================
+// Task 3
+// ====================
+{
+  let num = "10";
 
-// Solution One
-console.log(+num + +num); // 20
+  // Solution One
+  console.log(+num + +num); // 20
 
-// Solution Two
-console.log(Number(num) + Number(num)); // 20
+  // Solution Two
+  console.log(Number(num) + Number(num)); // 20
 
-// Solution Three
-console.log(parseInt(num) * true + parseInt(num)); // 20
+  // Solution Three
+  console.log(parseInt(num) * true + parseInt(num)); // 20
 
-// Solution Four
-console.log(num * (true + true)); // 20
+  // Solution Four
+  console.log(num * (true + true)); // 20
 
-// Solution Five
-console.log(num - -num); // 20
+  // Solution Five
+  console.log(num - -num); // 20
+}
 
-// task 4
-let points = 10;
+// ====================
+// Task 4
+// ====================
+{
+  let points = 10;
 
-// Write Your Code Here
-points += ++true + ++true + true;
+  points += 3;
 
-console.log(points); // 13
+  console.log(points); // 13
 
-// Write Your Code Here
-points -= --true + --true + true + true + true;
+  points -= 5;
 
-console.log(points); // 8
+  console.log(points); // 8
+}
