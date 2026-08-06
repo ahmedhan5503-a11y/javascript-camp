e9ce970676bf659d75dbdc87efdc24994cee3891
Ahmed Hany <ahmedhan5503@gmail.com>
2026-08-06 12:42:50 +0300

    finish tasks 23to26

--- task-23-26/script.js
+++ task-23-26/script.js
@@ -20,3 +20,44 @@ console.log(100_00 * 10); // 100000
 console.log(Number("100000")); // 100000
 
 console.log(parseInt("100000")); // 100000
+// ==================
+// task 2
+// ==================
+console.log(-Number.MIN_SAFE_INTEGER); // 9007199254740991
+// ==================
+// task 3
+// ==================
+console.log(Number.MAX_SAFE_INTEGER.toString().length); // 16
+// ==================
+// task 4
+// ==================
+let myVar = "100.56789 Views";
+
+console.log(parseInt(myVar)); // 100
+
+console.log(parseFloat(myVar).toFixed(2) * 1); // 100.57
+// ==================
+// task 5
+// ==================
+let num = 10;
+
+console.log(Number.isInteger(num) + Number.isInteger(num)); // 2
+
+// ==================
+// task 6
+// ==================
+let flt = 10.4;
+
+console.log(Math.floor(flt)); // 10
+
+console.log(Math.trunc(flt)); // 10
+
+console.log(Math.round(flt)); // 10
+
+console.log(parseInt(flt)); // 10
+
+console.log(flt.toFixed()); // 10
+// ==================
+// task 7
+// ==================
+console.log(Math.floor(Math.random() * 5));
