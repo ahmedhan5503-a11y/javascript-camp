7b13aa7d53f70dab13c54f594e99272c0a48c4be
Ahmed Hany <ahmedhan5503@gmail.com>
2026-08-08 12:53:27 +0300

    Complete tasks 27 to 30

---
+++ task-27-30/script.js
@@ -0,0 +1,41 @@
+let userName = "Elzero";
+
+// Solution One: charAt()
+console.log(userName.charAt(0).toLowerCase()); // e
+
+// Solution Two: []
+console.log(userName[0].toLowerCase()); // e
+
+// Solution Three: substring()
+console.log(userName.substring(0, 1).toLowerCase()); // e
+
+// Solution Four: slice()
+console.log(userName.slice(0, 1).toLowerCase()); // e
+
+// Solution Five: substr()
+console.log(userName.substr(0, 1).toLowerCase()); // e
+
+// Solution Six: repeat()
+console.log(userName[0].toLowerCase().repeat(3)); // eee
+
+
+// task 2
+let userName = "Elzero";
+
+// Solution One: charAt()
+console.log(userName.charAt(0).toLowerCase()); // e
+
+// Solution Two: []
+console.log(userName[0].toLowerCase()); // e
+
+// Solution Three: substring()
+console.log(userName.substring(0, 1).toLowerCase()); // e
+
+// Solution Four: slice()
+console.log(userName.slice(0, 1).toLowerCase()); // e
+
+// Solution Five: substr()
+console.log(userName.substr(0, 1).toLowerCase()); // e
+
+// Solution Six: repeat()
+console.log(userName[0].toLowerCase().repeat(3)); // eee
