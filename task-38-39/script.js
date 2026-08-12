fe9e0daa884d5989b24362259416fa2e5ef24186
Ahmed Hany <ahmedhan5503@gmail.com>
2026-08-12 10:03:49 +0300

    Complete tasks 38to 39

---
+++ task-38-39/script.js
@@ -0,0 +1,32 @@
+//task1
+let day = "   friday  ";
+
+// Remove spaces
+day = day.trim();
+
+// Make first letter Capital and the rest Small
+day = day.charAt(0).toUpperCase() + day.slice(1).toLowerCase();
+
+switch (day) {
+  case "Friday":
+  case "Saturday":
+  case "Sunday":
+    console.log("No Appointments Available");
+    break;
+
+  case "Monday":
+  case "Thursday":
+    console.log("From 10:00 AM To 5:00 PM");
+    break;
+
+  case "Tuesday":
+    console.log("From 10:00 AM To 6:00 PM");
+    break;
+
+  case "Wednesday":
+    console.log("From 10:00 AM To 7:00 PM");
+    break;
+
+  default:
+    console.log("Its Not A Valid Day");
+}
